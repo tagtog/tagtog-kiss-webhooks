be1e5013b00f358a73798aa14cd5a45eca4bd093
Juan Miguel Cejuela <i@juanmi.rocks>
2020-03-30 15:10:19 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # tagtog-kiss-webhooks
+
+## Requirements
+
+* Python 3.8+ (you can this with [pyenv](https://github.com/pyenv/pyenv) as in: `pyenv install 3.8.2`)
+* [pipenv](https://github.com/pypa/pipenv)
+
+## Install
+
+```shell
+# Prepare the virtualenv
+pipenv --python $(pyenv which python)
+# Enter in the shell
+pipenv shell
+# Install all dependencies
+pipenv sync
+```
+
+## Run
+
+```shell
+uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import asyncio
+import logging
+import time
+
 from fastapi import FastAPI, Header
 
 app = FastAPI()
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)-24s %(levelname)-8s %(name)-20s %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+
+def annotate():
+    return {
+        "anncomplete": False,
+        "annotatables": [],
+        "sources": [],
+        "metas": [],
+        "entities": [],
+        "relations": []
+    }
+
+
+async def success_async_annotate():
+    logger.info("started")
+    await asyncio.sleep(5)  # Wait some time to simulate a real ML prediction
+    logger.info("finished")
+
+
+# -----------------------------------------------------------------------------
 
 
 @app.get("/ping")
@@ -10,6 +41,15 @@ def ping():
 
 
 @app.post("/will-annotate-document", status_code=204)
-def will_annotate_document(x_tagtog_webhookid: str = Header(None), x_tagtog_source: str = Header(None), x_tagtog_owner: str = Header(None), x_tagtog_project: str = Header(None), x_tagtog_member: str = Header(None), x_tagtog_docid: str = Header(None), x_tagtog_jobid: str = Header(None)):
-    print(x_tagtog_webhookid)
+def will_annotate_document(x_tagtog_webhookid: str = Header(None),
+                           x_tagtog_source: str = Header(None),
+                           x_tagtog_owner: str = Header(None),
+                           x_tagtog_project: str = Header(None),
+                           x_tagtog_member: str = Header(None),
+                           x_tagtog_docid: str = Header(None),
+                           x_tagtog_jobid: str = Header(None)):
+
+    logger.info(x_tagtog_webhookid)
+    asyncio.run(success_async_annotate())
+
     return
