018f94099e3baf1ba7562f20df8d6ce66e6a7646
Juan Miguel Cejuela <i@juanmi.rocks>
2020-04-02 17:09:35 +0200

    implement way to respond to failure-async-annotate

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ def return_empty_annotations():
     }
 
 
-async def success_async_annotate(username: str, password: str, info: Info):
-    logger.info(f"started annotation: {info}")
+async def success_async_annotate(username: str, password: str, info: AsyncAnnotateParams):
+    logger.info(f"start success_async_annotate: {info}")
     await asyncio.sleep(5)  # Wait some time to simulate a real ML prediction
     endpoint = "-api/documents/jobs/v1/success-async-annotate"
 
@@ -62,12 +62,31 @@ async def success_async_annotate(username: str, password: str, info: Info):
         params=info,
         json=return_empty_annotations())
 
-    logger.info(f"{res.status_code} {res.text}")
-    logger.info("finished")
+    logger.info(f"end success_async_annotate: {res.status_code} {res.text}")
 
 
-async def async_annotate(username: str, password: str, info: Info):
-    await success_async_annotate(username, password, info)
+async def failure_async_annotate(username: str, password: str, info: AsyncAnnotateParams, error: str):
+    logger.info(f"start failure_async_annotate: {info}")
+    await asyncio.sleep(5)  # Wait some time to simulate a real ML prediction with error
+    endpoint = "-api/documents/jobs/v1/failure-async-annotate"
+
+    res = requests.post(
+        TAGTOG_DOMAIN + endpoint,
+        auth=HTTPBasicAuth(username, password),
+        verify=TAGTOG_SSL_CERTIFICATE,
+        params=info,
+        data=error)
+
+    logger.info(f"end failure_async_annotate: {res.status_code} {res.text}")
+
+
+async def async_annotate(username: str, password: str, info: AsyncAnnotateParams, error: str = None):
+    if error:
+        info.format = "error"
+        await failure_async_annotate(username, password, info, error)
+    else:
+        info.format = "ann.json"
+        await success_async_annotate(username, password, info)
 
 
 # -----------------------------------------------------------------------------
@@ -83,6 +102,10 @@ async def ping():
 async def will_annotate_document(
         username: str,
         password: str,
+        # Optional query parameter.
+        # If not given, our server will respond with "success-async-annotate"
+        # otherwise, our server will respond with "failure-async-annotate" returning the error string given
+        error: str = None,
         x_tagtog_webhookid: str = Header(None),
         x_tagtog_source: str = Header(None),
         x_tagtog_owner: str = Header(None),
@@ -91,7 +114,7 @@ async def will_annotate_document(
         x_tagtog_docid: str = Header(None),
         x_tagtog_jobid: str = Header(None)):
 
-    info = Info(
+    info = AsyncAnnotateParams(
         owner=x_tagtog_owner,
         project=x_tagtog_project,
         member=x_tagtog_member,
@@ -100,7 +123,7 @@ async def will_annotate_document(
         jobid=x_tagtog_jobid
     )
 
-    logger.info(x_tagtog_webhookid)
-    asyncio.create_task(async_annotate(username, password, info))
+    logger.info(f"will_annotate_document: {info}")
+    asyncio.create_task(async_annotate(username, password, info, error))
 
     return
