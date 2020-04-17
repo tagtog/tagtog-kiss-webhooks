acaa8eeada7071eff1955b8b5ea692d2af4b0600
Juan Miguel Cejuela <i@juanmi.rocks>
2020-04-17 15:47:02 +0200

    accept new param: delaySeconds

--- main.py
+++ main.py
@@ -50,24 +50,24 @@ def return_empty_annotations():
     }
 
 
-async def success_async_annotate(username: str, password: str, info: AsyncAnnotateParams):
-    logger.info(f"start success_async_annotate: {info}")
-    await asyncio.sleep(5)  # Wait some time to simulate a real ML prediction
+async def success_async_annotate(username: str, password: str, params: AsyncAnnotateParams, delaySeconds: int):
+    logger.info(f"start success_async_annotate: {params}")
+    await asyncio.sleep(delaySeconds)  # Wait some time to simulate a real ML prediction
     endpoint = "-api/documents/jobs/v0/success-async-annotate"
 
     res = requests.post(
         TAGTOG_DOMAIN + endpoint,
         auth=HTTPBasicAuth(username, password),
         verify=TAGTOG_SSL_CERTIFICATE,
-        params=info,
+        params=params,
         json=return_empty_annotations())
 
     logger.info(f"end success_async_annotate: {res.status_code} {res.text}")
 
 
-async def failure_async_annotate(username: str, password: str, info: AsyncAnnotateParams, error: str):
-    logger.info(f"start failure_async_annotate: {info}")
-    await asyncio.sleep(5)  # Wait some time to simulate a real ML prediction with error
+async def failure_async_annotate(username: str, password: str, params: AsyncAnnotateParams, error: str, delaySeconds: int):
+    logger.info(f"start failure_async_annotate: {params} -> {error}")
+    await asyncio.sleep(delaySeconds)  # Wait some time to simulate a real ML prediction with error
     endpoint = "-api/documents/jobs/v0/failure-async-annotate"
 
     res = requests.post(
@@ -83,13 +83,13 @@ async def failure_async_annotate(username: str, password: str, info: AsyncAnnota
     logger.info(f"end failure_async_annotate: {res.status_code} {res.text}")
 
 
-async def async_annotate(username: str, password: str, info: AsyncAnnotateParams, error: str = None):
+async def async_annotate(username: str, password: str, params: AsyncAnnotateParams, error: str, delaySeconds: int):
     if error:
-        info.format = "error"
-        await failure_async_annotate(username, password, info, error)
+        params.format = "error"
+        await failure_async_annotate(username, password, params, error, delaySeconds)
     else:
-        info.format = "ann.json"
-        await success_async_annotate(username, password, info)
+        params.format = "ann.json"
+        await success_async_annotate(username, password, params, delaySeconds)
 
 
 # -----------------------------------------------------------------------------
@@ -109,6 +109,7 @@ async def will_annotate_document(
         # If not given, our server will respond with "success-async-annotate"
         # otherwise, our server will respond with "failure-async-annotate" returning the error string given
         error: str = None,
+        delaySeconds: int = 5,
         x_tagtog_webhookid: str = Header(None),
         x_tagtog_source: str = Header(None),
         x_tagtog_owner: str = Header(None),
@@ -117,7 +118,7 @@ async def will_annotate_document(
         x_tagtog_docid: str = Header(None),
         x_tagtog_jobid: str = Header(None)):
 
-    info = AsyncAnnotateParams(
+    params = AsyncAnnotateParams(
         owner=x_tagtog_owner,
         project=x_tagtog_project,
         member=x_tagtog_member,
@@ -126,7 +127,7 @@ async def will_annotate_document(
         jobid=x_tagtog_jobid
     )
 
-    logger.info(f"will_annotate_document: {info}")
-    asyncio.create_task(async_annotate(username, password, info, error))
+    logger.info(f"will_annotate_document: {params}")
+    asyncio.create_task(async_annotate(username, password, params, error, delaySeconds))
 
     return
